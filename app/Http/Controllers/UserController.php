4a0392aba620cfac6df00b32dbda7bd523044183
José Victor <josevitorppessoa@gmail.com>
2022-07-26 18:30:26 -0300

    Creating views Show.blade and index.blade
    creting dthe views that will display the users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,24 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = [
-            'nomes' => [
-                'José Victor',
-                'Zézinhopessoa']
-        ];
+        $users = User::all();
 
-        dd($users);
+
+        return view('users.index', compact('users'));
     }
 
     public function show($id)
     {
-        dd('Id do Usuário é '. $id);
+
+        if (!$user = User::find($id))
+            return redirect()->route('users.index');
+        return view('users.show', compact('user'));
     }
+
 
 }
