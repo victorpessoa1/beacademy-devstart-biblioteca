ba7b037d348d46d9581af85a80c7dbb67447e2a3
José Victor <josevitorppessoa@gmail.com>
2022-08-06 10:02:20 -0300

    Creating the route and methods deleteUser
    API finished. CRUD concluded with the inclusion of the methods that delete a user from the database on UserController, creating and implementing the route to do so at routes/web and views/users/show

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
 Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
