6313e94be0e1120d966a0ea8b4df0f9627f26ca1
José Victor <josevitorppessoa@gmail.com>
2022-08-06 09:27:39 -0300

    Creating edit.blade.php in Users View
    Creating edit.blade.php in Users View and implementing it on UserController and routes. also creating a button on show.blade that leads to edit page to edit a user

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,8 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 Route::post('/user', [UserController::class, 'store'])->name('users.store');
